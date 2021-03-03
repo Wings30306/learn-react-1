dc93fdcd22948669c1620cd1d0742d347171a691
Joke Heyndels <jo_hannah@outlook.com>
2021-03-03 11:34:33 +0000

    Conditionals in JSX: ternary operator

--- JSX/index.js
+++ JSX/index.js
@@ -3,13 +3,24 @@ function getMood(){
     return moods[Math.floor(Math.random() * moods.length)]
 }
 
+function getNum(){
+    return Math.floor(Math.random() * 10) + 1;
+}
+
 class JSXDEMO extends React.Component {
     render(){
-        let username = "Jo"
+        let username = "Jo";
+        let num = getNum();
         return (
             <div>
-                <h1>My name is {username}</h1>
-                <h2>My current mood is {getMood()}</h2>
+                <h1>Nice to meet you</h1>
+                <ol>
+                    <li>My name is {username}</li>
+                    <li>My current mood is {getMood()}</li>
+                    <li>My lucky number is {num}</li>
+                </ol>
+                <p>{num === 7 || num === 3 ? "Congrats!" : "Bad luck, try again!"}</p>
+                
             </div>
         )
     }
