4b834b0f70d43322f296bc65b3424849a0a33278
Joke Heyndels <jo_hannah@outlook.com>
2021-03-03 11:14:29 +0000

    Passing in a variable in JSX

--- JSX/index.js
+++ JSX/index.js
@@ -1,7 +1,10 @@
 class JSXDEMO extends React.Component {
     render(){
+        let username = "Jo"
         return (
-            <img src="https://images.unsplash.com/photo-1593642634402-b0eb5e2eebc9?ixid=MXwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80" /> // MUST BE CLOSED!
+            <div>
+                <h1>My name is {username}</h1>
+            </div>
         )
     }
 }
