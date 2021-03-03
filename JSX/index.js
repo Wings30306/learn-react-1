748888e42b80df8f2f74c760bb5f402f39b44feb
Joke Heyndels <jo_hannah@outlook.com>
2021-03-03 11:18:50 +0000

    Add a function in JSX

--- JSX/index.js
+++ JSX/index.js
@@ -1,9 +1,15 @@
+function getMood(){
+    const moods = ["Angry", "Hungry", "Silly", "Quiet", "Happy", "Sleepy"];
+    return moods[Math.floor(Math.random() * moods.length)]
+}
+
 class JSXDEMO extends React.Component {
     render(){
         let username = "Jo"
         return (
             <div>
                 <h1>My name is {username}</h1>
+                <h2>My current mood is {getMood()}</h2>
             </div>
         )
     }
