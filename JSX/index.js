3cb855d2ca53103c9db5e592efaca181bd5a02f9
Joke Heyndels <jo_hannah@outlook.com>
2021-03-03 11:48:47 +0000

    Conditionals in JSX: shortcircuit

--- JSX/index.js
+++ JSX/index.js
@@ -20,7 +20,14 @@ class JSXDEMO extends React.Component {
                     <li>My lucky number is {num}</li>
                 </ol>
                 <p>{num === 7 || num === 3 ? "Congrats!" : "Bad luck, try again!"}</p>
-                
+                { 
+                num === 7 
+                ? <img src="https://media1.giphy.com/media/3HuKpHk6CW8A3ypdxv/giphy.gif?cid=ecf05e47fmvnjkdauiku3wzuctlyromj4v3f2x5hr3a61dx4&rid=giphy.gif"/> 
+                : null
+                }
+                {
+                    num === 3 && <img src="https://media1.giphy.com/media/3HuKpHk6CW8A3ypdxv/giphy.gif?cid=ecf05e47fmvnjkdauiku3wzuctlyromj4v3f2x5hr3a61dx4&rid=giphy.gif" />
+                }
             </div>
         )
     }
